90b838ccbb59ece4ef149c9fc56603fb995564ec
sudhanshujha2002 <143247363+sudhanshujha2002@users.noreply.github.com>
2023-08-26 11:50:24 +0530

    Covid Portfolio Project Using SQL.sql

--- Covid Portfolio Project Using SQL.sql
+++ Covid Portfolio Project Using SQL.sql
@@ -1,17 +1,18 @@
 /*
 Covid 19 Data Exploration 
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+Skills used: Joins, Alias, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
 
-Use PORTFOLIOPROJECT;
+--Creating the DataBase for our Project
+Create Database PORTFOLIOPROJECT
+Use PORTFOLIOPROJECT
 
---Data in the Covid Deaths
+--Data stored in the Covid Deaths.xlsx
 
 Select *
 From CovidDeaths
 Where continent is not null 
 order by 3,4
-
 
 -- Select Data that we are going to be starting with
 
@@ -20,8 +21,6 @@ From CovidDeaths
 Where continent is not null 
 order by 1,2
 
-
-
 -- Total Cases vs Total Deaths
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
@@ -29,13 +28,11 @@ From CovidDeaths
 Where continent is not null 
 order by 1,2
 
-
 -- Total Cases vs Population (Shows what percentage of population infected with Covid)
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From CovidDeaths
 order by 1,2
-
 
 -- Countries with Highest Infection Rate compared to Population
 
@@ -43,7 +40,6 @@ Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((to
 From CovidDeaths
 Group by Location, Population
 order by PercentPopulationInfected desc
-
 
 -- Countries with Highest Death Count per Population
 
@@ -53,8 +49,6 @@ Where continent is not null
 Group by Location
 order by TotalDeathCount desc
 
-
-
 -- BREAKING THINGS DOWN BY CONTINENT (Showing contintents with the highest death count per population)
 
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
@@ -63,14 +57,12 @@ Where continent is not null
 Group by continent
 order by TotalDeathCount desc
 
-
 -- GLOBAL NUMBERS
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From CovidDeaths
 where continent is not null 
 order by 1,2
-
 
 -- Total Population vs Vaccinations (Shows Percentage of Population that has recieved at least one Covid Vaccine)
 
@@ -83,7 +75,6 @@ Join CovidVaccinations as vac
 	and dea.date = vac.date
 where dea.continent is not null 
 order by 2,3
-
 
 -- Using CTE to perform Calculation on Partition By in previous query
 
@@ -104,8 +95,6 @@ where dea.continent is not null
 
 Select *, (RollingPeopleVaccinated/Population)*100
 From PopvsVac
-
-
 
 -- Using Temp Table to perform Calculation on Partition By in previous query
 
